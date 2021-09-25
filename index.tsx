659f9fce22406dcc7097b509e7fc47c466db62d7
rej70 <rej70@yahoo.com>
2021-09-25 15:52:39 -0400

    fixed random number generator function

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ const Array21 = () => {
     * @param max 
     * @returns random int - min & max inclusive
     */
-    getRandomInt(min, max) : number{
+    const getRandomInt = (min: number, max: number) => {
 	    min = Math.ceil(min);
 	    max = Math.floor(max);
 	    return Math.floor(Math.random() * (max - min + 1)) + min; 
